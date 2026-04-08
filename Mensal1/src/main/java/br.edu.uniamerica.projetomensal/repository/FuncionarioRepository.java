f24b77ed6cc49af4eb3327d33c1e2be3ac640b88
Adriano <adriannoordsj@gmail.com>
2026-04-08 19:58:29 -0300

    Repositories e modules pronto

--- Mensal1/src/main/java/br.edu.uniamerica.projetomensal/repository/FuncionarioRepository.java
+++ Mensal1/src/main/java/br.edu.uniamerica.projetomensal/repository/FuncionarioRepository.java
@@ -1,49 +1,46 @@
 package br.edu.uniamerica.projetomensal.repository;
 
-import br.edu.uniamerica.projetomensal.interfaces.Crud;
 import br.edu.uniamerica.projetomensal.model.Funcionario;
-import java.util.ArrayList;
+import jakarta.persistence.EntityManager;
+import br.edu.uniamerica.projetomensal.config.PersistenceManager;
+
 import java.util.List;
 
-// Classe repository, que implementa a interface Crud, e é responsável por realizar as operações de CRUD (Create, Read, Update, Delete) para a entidade Funcionario
-public class FuncionarioRepository implements Crud<Funcionario> {
-    // Crianco o "banco de dados" em memoria
-    // Criando a List do tipo Funcionario
-    private static List<Funcionario> funcionarios  = new ArrayList<>();
+// OBSOLETO - Classe repository, que implementa a interface Crud, e é responsável por realizar as operações de CRUD (Create, Read, Update, Delete) para a entidade Funcionario
+// Classe passa a receber persistencia EntityManager com JPA, sem ser por interface
+public class FuncionarioRepository {
 
-    // Sobreescrita dos metodos da interface Crud, para realizar as operacoes
+    private EntityManager em = PersistenceManager.getEntityManager();
 
-    @Override
     public void salvar(Funcionario funcionario) {
-        funcionarios.add(funcionario);
+        em.getTransaction().begin(); // Comeca a transacao
+        em.persist(funcionario);     // Salva no banco
+        em.getTransaction().commit();// Termina a operaaco
     }
 
-    // Apaga o funcionario da lista, caso o id seja encontrado, pelo numero do id, utilizando o metodo buscarPorId para encontrar
-    @Override
+    // Apaga o funcionario da lista, caso o id seja encontrado pelo numero do id, utilizando o metodo buscarPorId para encontrar
     public void excluir(int id) {
         Funcionario funcionario = buscarPorId(id);
         if (funcionario != null) {
-            funcionarios.remove(funcionario);
+            em.getTransaction().begin();
+            em.remove(funcionario);      // Remove o funcionario do banco
+            em.getTransaction().commit();
         }
     }
 
-    // Editar vazio, pois a edicao do funcionario pode ser feita diretamente na lista, utilizando o metodo buscarPorId para encontrar o funcionario e editar seus atributos
-    @Override
-    public void editar(Funcionario funcionario) {}
+    public void editar(Funcionario funcionario) {
+        em.getTransaction().begin();
+        em.merge(funcionario);       // Atualiza o estado da tabela no banco
+        em.getTransaction().commit();
+    }
 
-    // Busca o funcionario na lista por id com for each, caso nao tenha, retorna null
-    @Override
+    // Busca o funcionario na lista por id
     public Funcionario buscarPorId(int id) {
-        for (Funcionario funcionario : funcionarios) {
-            if (funcionario.getId() == id) {
-                return funcionario;
-            }
-        }
-        return null;
+        return em.find(Funcionario.class, id); // Procura na classe Funcionario, o id passado como parametro
     }
 
     // Lista todos os funcionarios da lista, utilizando o metodo listar para retornar a lista de funcionarios
     public List<Funcionario> listar() {
-        return funcionarios;
+        return em.createQuery("SELECT f FROM Funcionario f", Funcionario.class).getResultList();
     }
 }
