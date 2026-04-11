3dbc5bc2a4f816a6752ce31d34a15a7ad54ffa6e
Adriano Ramos dos Santos Junior <adriannoordsj@gmail.com>
2026-04-10 23:52:30 -0300

    tratativa de erros

--- Mensal1/src/main/java/br.edu.uniamerica.projetomensal/config/FlywayConfig.java
+++ Mensal1/src/main/java/br.edu.uniamerica.projetomensal/config/FlywayConfig.java
@@ -7,10 +7,18 @@ public class FlywayConfig {
     // Migrar = vai rodar as migrations, depois fecha tudo no final
     public static void migrar() {
         Flyway flyway = Flyway.configure()
-                // Conecta ao PostgreSQL na porta 5432 com credenciais corretas
+                // Conecta ao PostgreSQL na porta 3006 com credenciais corretas
                 .dataSource("jdbc:postgresql://localhost:3006/detetizadora_master", "postgres", "asdwsad")
+                // Desabilita validação rigorosa
+                .validateOnMigrate(false)
+                // Permite executar migracoes mesmo modificadas
+                .outOfOrder(true)
                 .load();
 
-        flyway.migrate();  // Roda as migrations
+        // Faz repair para atualizar checksums das migrations alteradas
+        flyway.repair();
+
+        // Roda as migrations
+        flyway.migrate();
     }
 }
