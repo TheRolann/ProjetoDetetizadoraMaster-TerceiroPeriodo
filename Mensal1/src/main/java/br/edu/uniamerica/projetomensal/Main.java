f229950d8aed329c11f01b7fe16b770a474dfffc
Adriano Ramos dos Santos Junior <adriannoordsj@gmail.com>
2026-05-08 21:36:44 -0300

    Limpando o projeto para iniciar com swing

---
+++ Mensal1/src/main/java/br/edu/uniamerica/projetomensal/Main.java
@@ -0,0 +1,11 @@
+package br.edu.uniamerica.projetomensal;
+
+import br.edu.uniamerica.projetomensal.config.PersistenceManager;
+import br.edu.uniamerica.projetomensal.config.FlywayConfig;
+
+public class Main {
+     public static void main (String[] args){
+        // Inicia o menu principal
+
+    }
+}
